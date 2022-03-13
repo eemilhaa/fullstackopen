41f5422859a17df686455f1ee063236600e96882
eemilhaa <eemil.haapanen@gmail.com>
2022-03-13 16:11:30 +0200

    1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 
-const Button = (props) => {
-  console.log(props)
-  const { handleClick, text } = props
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
+// Component for buttons
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+// Component for headers
+const Header = ({ content }) => (
+  <div>
+    <h1>{content}</h1>
+  </div>
+)
+// Component for displaying stats
+const DisplayStat = ({ stat, text }) => (
+  <div>
+    <p>{text} {stat}</p>
+  </div>
+)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,6 +25,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  // functions for changing state
   const addGood = () => {
     setGood(good + 1)
   }
@@ -28,12 +38,14 @@ const App = () => {
 
   return (
     <div>
+      <Header content="give feedback" />
       <Button handleClick={addGood} text="good" />
       <Button handleClick={addNeutral} text="neutral" />
       <Button handleClick={addBad} text="bad" />
-      {good}
-      {neutral}
-      {bad}
+      <Header content="statistics" />
+      <DisplayStat stat={good} text="good" />
+      <DisplayStat stat={neutral} text="neutral" />
+      <DisplayStat stat={bad} text="bad" />
     </div>
   )
 }
