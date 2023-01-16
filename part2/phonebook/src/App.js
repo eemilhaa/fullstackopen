d745c1a96bf32a0f15c0121a885341792180c56f
eemilhaa <eemil.haapanen@gmail.com>
2023-01-16 16:29:36 +0000

    2.15 & 2.16: add persons to server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios"
+import personService from "./services/persons"
 
 const Person = ({ person }) => {
   return (
@@ -55,14 +55,13 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
 
-  const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+  useEffect(() => {
+    personService
+      .getAll()
+      .then((initialPersons) => {
+        setPersons(initialPersons)
       })
-  }
-  useEffect(hook, [])
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -74,10 +73,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat(personObject));
-    setNewName("");
-    setNewNumber("");
-  };
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+      })
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
