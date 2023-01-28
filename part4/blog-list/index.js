a523e0b793213c1c7cbb8b7798f3c715818fb4b6
eemilhaa <eemil.haapanen@gmail.com>
2023-01-28 15:15:14 +0000

    4.2: add config and move app to app.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,47 +1,7 @@
-const http = require("http")
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
+const app = require("./app")
 const logger = require("./utils/logger")
-require("dotenv").config()
+const config = require("./utils/config")
 
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
-
-mongoose.set("strictQuery", false)
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/api/blogs", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
