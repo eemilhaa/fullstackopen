a1658d191488e65d3a7d78174a1b074bd5493792
eemilhaa <eemil.haapanen@gmail.com>
2023-01-28 16:20:33 +0200

    4.1: make project work

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -13,8 +17,8 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
+mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -37,7 +41,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
