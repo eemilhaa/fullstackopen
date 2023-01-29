2421bb42a700a6d4947e43d1f39ca213d6db6ca0
eemilhaa <eemil.haapanen@gmail.com>
2023-01-29 16:38:48 +0200

    4.2: add controllers and models

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,38 +4,17 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const logger = require("./utils/logger")
 const config = require("./utils/config")
+const blogRouter = require("./controllers/blogs")
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
 
 mongoose.set("strictQuery", false)
 mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info("connected to mongodb"))
+  .catch((error) => logger.error("error connecting", error.message))
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/blogs", blogRouter)
 
-app.get("/api/blogs", (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
