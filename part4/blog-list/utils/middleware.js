34a0956e3b0a3acaf9d4f97aaf97384a361cff3f
eemilhaa <eemil.haapanen@gmail.com>
2023-01-31 17:06:41 +0000

    make errorHandler better

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,12 +1,13 @@
 const logger = require("../utils/logger")
 
 const errorHandler = (error, request, response, next) => {
-  if (error.name === 'ValidationError') {
+  if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" })
   } else {
     logger.error(error)
   }
-
   next(error)
 }
 
