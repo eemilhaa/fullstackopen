b14d64132e17dc5c81eb8761c59be3a01164330e
eemilhaa <eemil.haapanen@gmail.com>
2023-01-31 11:32:18 +0000

    4.10: add tests for POST

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -33,6 +33,30 @@ describe("GET", () => {
   })
 })
 
+describe("POST", () =>{
+  test('a valid blog can be added ', async () => {
+    const newBlog = {
+      title: 'async/await simplifies making async calls',
+      author: "A. Uthor",
+      url: "asdf",
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(response.body).toHaveLength(2 + 1)
+    expect(titles).toContain(
+      'async/await simplifies making async calls'
+    )
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
