99f7ee79f4c679ee0d7bc7aa1898469c25b000e4
eemilhaa <eemil.haapanen@gmail.com>
2023-02-08 13:09:53 +0000

    5.1 add loginservice

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from "react"
 import Blog from "./components/Blog"
 import Login from "./components/Login"
 import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,9 +16,19 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    console.log("logging in with", username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      console.log("error")
+    }
   }
 
   const handleUsernameChange = ({ target }) => setUsername(target.value)
@@ -24,10 +36,6 @@ const App = () => {
   
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
       <h2>Login</h2>
         <Login
           username={username}
@@ -36,6 +44,10 @@ const App = () => {
           handleUsernameChange={handleUsernameChange}
           handleLogin={handleLogin}
         />
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   )
 }
