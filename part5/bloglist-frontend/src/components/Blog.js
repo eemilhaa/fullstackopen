0b8ce7c2ba5607f75b8804a9abe707e8677ef2b5
eemilhaa <eemil.haapanen@gmail.com>
2023-03-03 12:55:30 +0000

    5.7 (& 5.8 works too somehow): togglable blog details

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    padding: 10,
+    border: 'solid',
+    borderWidth: 1,
+    borderColor: "lightgray",
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {showDetails &&
+        <div>
+          {blog.title}
+          <button onClick={toggleShowDetails}>hide</button>
+          <br/>
+          {blog.url} <br/>
+          {blog.likes}
+          <button onClick={() => console.log("like")}>like</button>
+          <br/>
+          {blog.author} <br/>
+        </div>
+      }
+      {!showDetails &&
+        <div>
+          {blog.title}
+          <button onClick={toggleShowDetails}>view</button>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
