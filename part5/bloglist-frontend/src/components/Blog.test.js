9d6c2630f07bedb85a5f477bea87416f8f724b76
eemilhaa <eemil.haapanen@gmail.com>
2023-06-15 07:57:50 +0000

    5.14: test url and likes are shown

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,7 +1,9 @@
 import Resact from "react"
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
+import { exact } from "prop-types"
 
 const mockUser = {
   id: 123
@@ -9,15 +11,16 @@ const mockUser = {
 
 const mockService = jest.fn()
 const mockSetter = jest.fn()
+const blog = {
+  id: "1",
+  title: "blog",
+  author: "writer",
+  url: "url123",
+  user: mockUser,
+  likes: 10,
+}
 
 test("renders content", () => {
-  const blog = {
-    id: "1",
-    title: "blog",
-    author: "writer",
-    url: "123",
-    user: mockUser,
-  }
   render(<Blog
     key={blog.id}
     blog={blog}
@@ -28,4 +31,22 @@ test("renders content", () => {
   />)
   const title = screen.getByText("blog")
   expect(title).toBeDefined()
+})
+
+test("renders url, likes", async () => {
+  render(<Blog
+    key={blog.id}
+    blog={blog}
+    blogService={mockService}
+    blogs={[]}
+    setBlogs={mockSetter}
+    user={mockUser}
+  />)
+  const user = userEvent.setup()
+  const button = screen.getByText("view")
+  await user.click(button)
+  const url = screen.findByText("url123")
+  const likes = screen.findByText("10")
+  expect(url).toBeDefined()
+  expect(likes).toBeDefined()
 })
